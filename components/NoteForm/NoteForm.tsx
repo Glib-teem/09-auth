f87dc845a53cf09d0dcf55502412bfc9fd2f8a0a
Glib-xx <sevensky.ga@gmail.com>
2025-10-18 11:21:30 +0300

    update components

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect, type ChangeEvent, type FormEvent } from 'react';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
-import { createNote } from '@/lib/api';
+import { createNote } from '@/lib/api/clientApi';
 import { useNoteStore } from '@/lib/store/noteStore';
 import css from './NoteForm.module.css';
 
